a7548c7473a8a386c5073bed64b1f17a202e84af
ganbin <jlau.ice@gmail.com>
2025-10-27 19:29:07 +0800

    架构补充

--- src/router/menus.ts
+++ src/router/menus.ts
@@ -15,9 +15,40 @@ export const menus: Array<RouteRecordRaw> = [
         component: () => import('@/views/home/Home.vue'),
         meta: {
             id: uuid(),
-            icon: 'House',
+            icon: 'HomeFilled',
         }
     },
+    {
+        name: '系统管理',
+        path: '/sys',
+        component: () => import('@/views/home/Home.vue'),
+        children: [
+            {
+                name: '用户管理',
+                path: '/user/manage',
+                component: () => import('@/views/user/UserManage.vue'),
+                meta: {
+                    id: uuid(),
+                    icon: 'UserFilled',
+                }
+            },
+            {
+                name: '字段管理',
+                path: '/dict',
+                component: () => import('@/views/user/DictManage.vue'),
+                meta: {
+                    id: uuid(),
+                    icon: 'Lock',
+                }
+            },
+        ],
+        meta: {
+            id: uuid(),
+            icon: 'HomeFilled',
+        }
+    },
+
+    // 你所需要修改的路由
     {
         name: '任务管理',
         path: '/task/manage',
@@ -47,4 +78,5 @@ export const menus: Array<RouteRecordRaw> = [
             icon: 'Refresh',
         }
     },
+
 ]
