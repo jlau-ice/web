2224010e3186257a2e4a48b9c5b07ca09867807c
ganbin <jlau.ice@gmail.com>
2025-10-28 12:37:30 +0800

    国际化

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,18 @@ import ArcoVueIcon from '@arco-design/web-vue/es/icon'
 import { createPinia } from 'pinia'
 import router from './router'
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-
 import '@/request'
 const pinia = createPinia()
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
+app.use(ElementPlus, {
+    locale: zhCn,
+})
 app.use(ElementPlus)
 app.use(ArcoVueIcon)
 app.use(ArcoVue)
